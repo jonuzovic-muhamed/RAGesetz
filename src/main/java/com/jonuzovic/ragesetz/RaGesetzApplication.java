1a13399d3c7226e01419cbdb5355af9ae17d9fd6
Muhamed Jonuzovic <jon.muha@mailbox.org>
2025-10-15 20:31:19 +0200

    Initial commit.

---
+++ src/main/java/com/jonuzovic/ragesetz/RaGesetzApplication.java
@@ -0,0 +1,12 @@
+package com.jonuzovic.ragesetz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaGesetzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaGesetzApplication.class, args);
+	}
+}
