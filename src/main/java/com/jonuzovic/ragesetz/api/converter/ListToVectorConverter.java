0af513d7d54c2aae8f822b01f0d9d190b5d6a7aa
Muhamed Jonuzovic <jon.muha@mailbox.org>
2025-10-22 00:36:17 +0200

    Implement ListToVectorConverter for pgvector mapping to List<Float> and vice versa
    -Implemented the class ListToVectorConverter with method for converting
    to List and PGVector as String in form of Float array.
    -Implemented a simple unit test with edge cases to test out the
    converter.

---
+++ src/main/java/com/jonuzovic/ragesetz/api/converter/ListToVectorConverter.java
@@ -0,0 +1,46 @@
+package com.jonuzovic.ragesetz.api.converter;
+
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class ListToVectorConverter {
+
+    public String convertToDatabaseColumn(List<Float> list) {
+        if (list == null || list.isEmpty()) return "[]";
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("[");
+        for (int i = 0; i < list.size(); i++) {
+        	if (i == list.size() - 1) {
+                stringBuilder.append(list.get(i));
+                break;
+        	}
+            stringBuilder.append(list.get(i));
+            stringBuilder.append(",");
+        }
+        stringBuilder.append("]");
+        return stringBuilder.toString();
+    }
+
+    public List<Float> convertToEntityAttribute(String dbData) {
+        if ("[]".equals(dbData) || dbData == null || dbData.isBlank()
+        		|| !dbData.startsWith("[") || !dbData.endsWith("]") || dbData.contains(",,")) 
+        	return List.of();
+        
+        String cleaned = dbData.replace("[", "").replace("]", "");
+        String[] parts = cleaned.split(",");
+        
+        if (parts.length != 1536) {
+        	return List.of();
+        }
+        
+        List<Float> result = new ArrayList<>();
+        for (String part : parts) {
+            result.add(Float.parseFloat(part.trim()));
+        }
+        
+        return result;
+    }
+}
---
+++ src/test/java/com/jonuzovic/ragesetz/api/converter/ListToVectorConverterUnitTest.java
@@ -0,0 +1,91 @@
+package com.jonuzovic.ragesetz.api.converter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class ListToVectorConverterUnitTest {
+
+	ListToVectorConverter converter = new ListToVectorConverter();
+
+	private static final int VECTOR_DIM = 1536;
+
+	private static List<Float> asList = new ArrayList<Float>();
+	private static float[] asArray = new float[VECTOR_DIM];
+	private static String asString = new String();
+
+	@BeforeAll
+	static void setupVectorData() {
+		Random random = new Random();
+		asString += "[";
+		for (int i = 0; i < VECTOR_DIM; i++) {
+			float value = random.nextFloat();
+			asList.add(value);
+			asArray[i] = value;
+			if (i == VECTOR_DIM - 1) {
+				asString += value;
+			} else {
+				asString += value + ",";				
+			}
+		}
+		asString += "]";
+	}
+
+	@Test void testConvertToDatabaseColumn() { 
+		assertEquals(asString, converter.convertToDatabaseColumn(asList));
+	}
+	
+	@Test
+	void testConvertToDatabaseColumnWithNull() {
+		assertEquals("[]", converter.convertToDatabaseColumn(null));
+	}
+	
+	@Test
+	void testConvertToDatabaseColumnWithEmptyList() {
+		assertEquals("[]", converter.convertToDatabaseColumn(List.of()));
+	}
+	
+	@Test
+	void testConvertToEntityAttribute() {
+		assertEquals(asList, converter.convertToEntityAttribute(asString));
+		assertEquals(asList.get(VECTOR_DIM - 1), converter.convertToEntityAttribute(asString).get(VECTOR_DIM - 1));
+	}
+	
+	@Test
+	void testConvertToEntityAttributeWithNull() {
+		assertEquals(List.of(), converter.convertToEntityAttribute(null));
+	}
+	
+	@Test
+	void testConvertToEntityAttributeWithEmptyString() {
+		assertEquals(List.of(), converter.convertToEntityAttribute(""));
+	}
+	
+	@Test
+	void testConvertToEntityAttributeWithWrongFormatVector() {
+		assertEquals(List.of(), converter.convertToEntityAttribute("0.3213,1.000,4.324]"));
+		assertEquals(List.of(), converter.convertToEntityAttribute("[0.3213,1.000,4.324"));
+		assertEquals(List.of(), converter.convertToEntityAttribute("0.3213,1.000,4.324"));
+		assertEquals(List.of(), converter.convertToEntityAttribute("[0.3213,1.000,4.324,]"));
+	}
+	
+	@Test
+	void testConvertToEntityAttributeWithTooShortVector() {
+		assertEquals(List.of(), converter.convertToEntityAttribute("[0.3213,1.000,4.324]"));
+	}
+	
+	@Test
+	void testConvertToEntityAttributeWithTooManyCommas() {
+		StringBuilder wrongFormat = new StringBuilder();
+		wrongFormat.append(asString);
+		wrongFormat.deleteCharAt(wrongFormat.length()-1);
+		wrongFormat.append(",,,,]");
+		assertEquals(List.of(), converter.convertToEntityAttribute(wrongFormat.toString()));
+	}
+	
+}
