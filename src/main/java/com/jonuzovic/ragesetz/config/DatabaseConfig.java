4a3c664b9ed4b0a15811deee35ec683ae111ac64
Muhamed Jonuzovic <jon.muha@mailbox.org>
2025-10-21 20:40:19 +0200

    Add DatabaseConfig to provide JdbcTemplate bean for injection
    A bean of JbdcTemplate is created with the apps DataSource which spring
    fills in with provided jdbc parameters. This allows for injection of
    JdbcTemplate whereever needed from IOC container.

---
+++ src/main/java/com/jonuzovic/ragesetz/config/DatabaseConfig.java
@@ -0,0 +1,16 @@
+package com.jonuzovic.ragesetz.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Bean
+    JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+}
