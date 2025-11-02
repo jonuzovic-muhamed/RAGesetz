595d2171265bbc00b0847450caf9ee6fee08117a
Muhamed Jonuzovic <jon.muha@mailbox.org>
2025-11-02 21:21:37 +0100

    Create ILawEmbedder.java
    Defined behaviour for the law embedder.

---
+++ src/main/java/com/jonuzovic/ragesetz/core/embedder/ILawEmbedder.java
@@ -0,0 +1,11 @@
+package com.jonuzovic.ragesetz.core.embedder;
+
+import java.util.List;
+
+import com.jonuzovic.ragesetz.core.model.Law;
+
+public interface ILawEmbedder {
+
+	void embedLaws(List<Law> laws);
+	
+}
